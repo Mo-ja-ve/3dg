c772baa45eea5676795ab8d8480ec2620eb25573
unknown <ak461717@ohio.edu>
2022-04-12 21:52:59 -0400

    pointer pointer vertext list

--- main.cpp
+++ main.cpp
@@ -46,12 +46,12 @@ struct HLineList{
 
 int main(int argc, char** argv){
 
-    vertList triVertexList;
-    //triVertexList += 3;
-    triVertexList.vertPtr +=3;
-    triVertexList.vertPtr[0].x = 100.0; triVertexList.vertPtr[0].y = 200.0; 
-    triVertexList.vertPtr[1].x = 400.0; triVertexList.vertPtr[1].y = 300.0; 
-    triVertexList.vertPtr[2].x = 175.0; triVertexList.vertPtr[2].y = 500.0; 
+    vertList *triVertexList;
+    triVertexList->vertPtr = (struct vert *) (malloc(sizeof(struct vert) * 3));
+
+    triVertexList->vertPtr[0].x = 100.0; triVertexList->vertPtr[0].y = 200.0;
+    triVertexList->vertPtr[1].x = 400.0; triVertexList->vertPtr[1].y = 300.0;
+    triVertexList->vertPtr[2].x = 175.0; triVertexList->vertPtr[2].y = 500.0;
 
     if(SDL_Init(SDL_INIT_VIDEO) < 0){
         printf("Error: SDL failed to initialize\nSDL Error: '%s'\n", SDL_GetError());
@@ -75,14 +75,14 @@ int main(int argc, char** argv){
     float movePoint = 0;
     int frameCount = 1;
 
-    //vert *VertexPtr = triVertexList->vertPtr;
+    vert *VertexPtr = triVertexList->vertPtr;
     int i = 0;
     float MinIndexL, MaxIndex, MinIndexR, SkipFirst, Temp;
     float MinPoint_Y, MaxPoint_Y, TopIsFlat, LeftEdgeDir;
 
     struct HLineList WorkingHLineList;
     struct HLine *EdgePointPtr;
-    struct Point *VertexPtr;
+    //struct Point *VertexPtr;
 
 
     while(running){
@@ -102,31 +102,32 @@ int main(int argc, char** argv){
         SDL_RenderClear(renderer);
 
         SDL_SetRenderDrawColor(renderer, 0, 200, 200, 200);
-        SDL_RenderDrawLineF(renderer, movePoint+triVertexList.vertPtr[0].x, movePoint+triVertexList.vertPtr[0].y, movePoint+triVertexList.vertPtr[1].x, movePoint+triVertexList.vertPtr[1].y);
-        SDL_RenderDrawLineF(renderer, movePoint+triVertexList.vertPtr[1].x, movePoint+triVertexList.vertPtr[1].y, movePoint+triVertexList.vertPtr[2].x, movePoint+triVertexList.vertPtr[2].y);
-        SDL_RenderDrawLineF(renderer, movePoint+triVertexList.vertPtr[2].x, movePoint+triVertexList.vertPtr[2].y, movePoint+triVertexList.vertPtr[0].x, movePoint+triVertexList.vertPtr[0].y);
+        SDL_RenderDrawLineF(renderer, movePoint+triVertexList->vertPtr[0].x, movePoint+triVertexList->vertPtr[0].y, movePoint+triVertexList->vertPtr[1].x, movePoint+triVertexList->vertPtr[1].y);
+        SDL_RenderDrawLineF(renderer, movePoint+triVertexList->vertPtr[1].x, movePoint+triVertexList->vertPtr[1].y, movePoint+triVertexList->vertPtr[2].x, movePoint+triVertexList->vertPtr[2].y);
+        SDL_RenderDrawLineF(renderer, movePoint+triVertexList->vertPtr[2].x, movePoint+triVertexList->vertPtr[2].y, movePoint+triVertexList->vertPtr[0].x, movePoint+triVertexList->vertPtr[0].y);
 
 
-        // polygon horizontal fill algorithm
-        // pg 528
-        // MinIndexL = MaxIndex = 0;// scan list to find the top vertex
-        // MaxPoint_Y = MinPoint_Y = VertexPtr[0].y;
-        // for (i = 1; i < 3; i++) {
-        // if (VertexPtr[i].y < MinPoint_Y){
-        //     MinIndexL = i;
-        //     MinPoint_Y = VertexPtr[i].y; /* new top */
-        // } else if (VertexPtr[i].y > MaxPoint_Y){
-        //      MaxIndex = i;
-        //      MaxPoint_Y = VertexPtr[i].y; /* new bottom */
-        //     }
-        // }
+       // polygon horizontal fill algorithm
+        //pg 528
+        MinIndexL = MaxIndex = 0;// scan list to find the top vertex
+        MaxPoint_Y = MinPoint_Y = VertexPtr[0].y;
+        for (i = 1; i < 3; i++) {
+        if (VertexPtr[i].y < MinPoint_Y){
+            MinIndexL = i;
+            MinPoint_Y = VertexPtr[i].y; /* new top */
+        } else if (VertexPtr[i].y > MaxPoint_Y){
+             MaxIndex = i;
+             MaxPoint_Y = VertexPtr[i].y; /* new bottom */
+            }
+        }
 
-        // if (MinPoint_Y == MaxPoint_Y)
-        // return(1); /* polygon is 0-height; avoid infinite loop below */
+        if (MinPoint_Y == MaxPoint_Y)
+        return(1); /* polygon is 0-height; avoid infinite loop below */
 
         if (frameCount == 50){
             frameCount = 0;
             movePoint++;
+            //cout<<"max point: "<<MaxPoint_Y;
         }
         frameCount++;
 
