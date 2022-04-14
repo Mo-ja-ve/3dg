0612813a4048c2c6f44dcaae787a386baaeb4e1a
unknown <ak461717@ohio.edu>
2022-04-13 20:10:31 -0400

    g++ optimizer bug

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
 all:
 	g++ -Iheaders/Include -Lheaders/lib -o main main.cpp -lmingw32 -lSDL2main -lSDL2
+
+debug:
+	g++ -Iheaders/Include -Lheaders/lib -o main -g main.cpp -lmingw32 -lSDL2main -lSDL2
--- main.cpp
+++ main.cpp
@@ -50,7 +50,7 @@ int main(int argc, char** argv){
     triVertexList->vertPtr = (struct vert *) (malloc(sizeof(struct vert) * 3));
 
     triVertexList->vertPtr[0].x = 100.0; triVertexList->vertPtr[0].y = 200.0;
-    triVertexList->vertPtr[1].x = 400.0; triVertexList->vertPtr[1].y = 300.0;
+    triVertexList->vertPtr[1].x = 400.0; triVertexList->vertPtr[1].y = 200.0;
     triVertexList->vertPtr[2].x = 175.0; triVertexList->vertPtr[2].y = 500.0;
 
     if(SDL_Init(SDL_INIT_VIDEO) < 0){
@@ -75,10 +75,11 @@ int main(int argc, char** argv){
     float movePoint = 0;
     int frameCount = 1;
 
-    vert *VertexPtr = triVertexList->vertPtr;
+    vert *VertexPtr = (struct vert *) (malloc(sizeof(triVertexList->vertPtr)));
+    VertexPtr = triVertexList->vertPtr;
+
     float MinPoint_Y, MaxPoint_Y;
-    int leftEdgeDir, TopIsFlat, LeftEdgeDir,  MinIndexL, MaxIndex, MinIndexR, SkipFirst, temp;
-    int i = 0;
+    int leftEdgeDir, TopIsFlat, LeftEdgeDir,  MinIndexL, MaxIndex, MinIndexR, SkipFirst, temp = 0;
 
     struct HLineList WorkingHLineList;
     struct HLine *EdgePointPtr;
@@ -112,9 +113,13 @@ int main(int argc, char** argv){
 
        // polygon horizontal fill algorithm
         //pg 528
-        MinIndexL = MaxIndex = 0;// scan list to find the top vertex
-        MaxPoint_Y = MinPoint_Y = VertexPtr[0].y;
-        for (i = 1; i < 3; i++) {
+        // scan list to find the top vertex
+        MinIndexR = 0;
+        MaxIndex = 0;
+        MinIndexL = 0;
+        MaxPoint_Y = VertexPtr[0].y;
+        MinPoint_Y = VertexPtr[0].y;
+        for (int i = 1; i < 3; i++) {
         if (VertexPtr[i].y < MinPoint_Y){
             MinIndexL = i;
             MinPoint_Y = VertexPtr[i].y; /* new top */
@@ -126,30 +131,32 @@ int main(int argc, char** argv){
 
         if (MinPoint_Y == MaxPoint_Y)
             return(1); /* polygon is 0-height; avoid infinite loop below */
-
+        
         /* Scan in ascending order to find the last top-edge point */
-        MinIndexR = MinIndexL;
-        while (VertexPtr[MinIndexR].y == MinPoint_Y)
-            INDEX_FORWARD(MinIndexR);
-        INDEX_BACKWARD(MinIndexR); /* back up to last top-edge point */
-        /* Now scan in descending order to find the first top-edge point */
-        while (VertexPtr[MinIndexL].y == MinPoint_Y)
-            INDEX_BACKWARD(MinIndexL);
-        INDEX_FORWARD(MinIndexL); 
 
-        // find wich edge from top vertex is left edge, right edge
+
+      //  while (VertexPtr[MinIndexR].y == MinPoint_Y)
+    //        INDEX_FORWARD(MinIndexR);
+  //      INDEX_BACKWARD(MinIndexR); /* back up to last top-edge point */
+        /* Now scan in descending order to find the first top-edge point */
+        // while (VertexPtr[MinIndexL].y == MinPoint_Y)
+        //     INDEX_BACKWARD(MinIndexL);
+        // INDEX_FORWARD(MinIndexL); 
+
+            // // find wich edge from top vertex is left edge, right edge
         LeftEdgeDir = -1;
-        if ((TopIsFlat = (VertexPtr[MinIndexL].x != VertexPtr[MinIndexR].x) ? 1 : 0) == 1){
-            if (VertexPtr[MinIndexL].x > VertexPtr[MinIndexR].x){
-                LeftEdgeDir = 1;
-                temp = MinIndexL;
-                MinIndexL = MinIndexR;
-                MinIndexR = temp;
-            }
-        } else{
+        // if ((TopIsFlat = (VertexPtr[MinIndexL].x != VertexPtr[MinIndexR].x) ? 1 : 0) == 1){
+        //     if (VertexPtr[MinIndexL].x > VertexPtr[MinIndexR].x){
+        //         LeftEdgeDir = 1;
+        //         temp = MinIndexL;
+        //         MinIndexL = MinIndexR;
+        //         MinIndexR = temp;
+        //         cout<<"HELLLLo!" << endl;
+        //     }
+        // } else{
             
 
-        }
+        // }
 
         if (frameCount == 50){
             frameCount = 0;
